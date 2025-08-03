db1fc8e58af1354d7678dc7e19e8cb764fa566ad
Abhay Bansal <targeter001@gmail.com>
2025-08-03 15:30:58 +0530

    updated content 1

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "@/component/Header";
+import React from "react";
+
+const Home2 = () => {
+  return (
+    <div className="w-full max-h-[500px] flex flex-col  pt-[200px]">
+      <Header />
+      <p className="p-44 pt-4  pb-6 text-center">
+        We design battery chips with precision and purpose — powering the future
+        through quiet intelligence.
+      </p>
+    </div>
+  );
+};
+
+export default Home2;
---
+++ src/component/Header.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import FlipLink from "./components/ui/text-effet-flipper"
+
+// const Header = () => {
+//   return <div className="bg-white">Header</div>;
+//   <FlipLink href="https://x.com/guri_who">Behance</FlipLink>
+//   <FlipLink href="https://x.com/guri_who">Behance</FlipLink>
+//   <FlipLink href="https://x.com/guri_who">Behance</FlipLink>
+// };
+
+// export default Header;
+
+import React from "react";
+
+const Header = () => {
+  return <div className=" h-[100px]">Header</div>;
+};
+
+export default Header;
