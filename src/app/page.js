fe0f35cd3118d53a87c1c87dee96b116b3fd19b7
Gaurav <gauravcoder3512@gmail.com>
2025-08-03 21:44:29 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,25 @@
 import Home2 from "@/Pages/Home";
 import Products from "@/component/Products";
-
-import { TrustedPartnersScroll } from "../components/ui/text-scroll"
+import { TrustedPartnersScroll } from "../components/ui/text-scroll";
 
 export default function Home() {
   return (
     <div className="max-w-7xl h-auto mx-auto">
       <Home2 />
-
-<<<<<<< HEAD
-      <div className="bg-black w-full min-h-[500px]">
+        <div className="w-full min-h-[500px] bg-black flex items-center justify-center">
         <video
+          className="w-full h-full object-cover"
           autoPlay
-          loop
           muted
+          loop
           playsInline
-          className=" w-full h-full object-cover z-0"
         >
           <source src="/bgv1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-=======
-     <div
-  className="bg-black w-full min-h-[500px]"
-  style={{
-    backgroundImage: "url('path/to/image.jpg')",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }}
-></div>
-
->>>>>>> 1b122dc7dce7cc8a0836e15868de6fd7d2222337
+      <Products />
+      <TrustedPartnersScroll />
     </div>
   );
 }
